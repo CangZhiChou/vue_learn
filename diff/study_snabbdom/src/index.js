60da4c08967260e29d91b6e6511456e23f594785
ykang2020 <yk1123@vip.163.com>
2021-04-14 16:21:12 +0800

    完成判断新旧节点text不同的情况

--- diff/study_snabbdom/src/index.js
+++ diff/study_snabbdom/src/index.js
@@ -4,13 +4,19 @@ import patch from "./my_snabbdom/patch";
 let container = document.getElementById("container");
 let btn = document.getElementById("btn");
 
-// const myVnode1 = h("h1", {}, "你好");
+const myVnode1 = h("h1", {}, "你好");
 
-const myVnode1 = h("ul", {}, [
+// 上树
+patch(container, myVnode1);
+
+const myVnode2 = h("ul", {}, [
   h("li", {}, "A"),
   h("li", {}, "B"),
   h("li", {}, "C"),
   h("li", {}, "D"),
 ]);
-// 上树
-patch(container, myVnode1);
+
+btn.onclick = function () {
+  patch(myVnode1, myVnode2);
+}
+
