21a3f5910f16b8708990a7c56f5c7648c4aeecac
ykang2020 <yk1123@vip.163.com>
2021-05-01 10:34:38 +0800

    更新readme

--- directive/my_directive/src/Compile.js
+++ directive/my_directive/src/Compile.js
@@ -6,9 +6,10 @@ export default class Compile {
     this.$vue = vue;
     // 挂载点
     this.$el = document.querySelector(el);
+
     // 如果用户传入了挂载点
     if (this.$el) {
-      // 调用函数，让节点变为fragment，类似于mustache中的tokens。实际上用的是AST，这里就是轻量级的，fragment
+      // 调用函数，让节点变为fragment，类似于mustache中的tokens。实际上用的是AST，这里是轻量级的，fragment
       let $fragment = this.node2Fragment(this.$el);
       // 编译
       this.compile($fragment);
@@ -16,32 +17,31 @@ export default class Compile {
       this.$el.appendChild($fragment);
     }
   }
-  node2Fragment(el) {
-    var fragment = document.createDocumentFragment();
 
-    var child;
-    // 让所有DOM节点，都进入fragment
+  node2Fragment(el) {
+    let fragment = document.createDocumentFragment();
+    let child;
     while ((child = el.firstChild)) {
       fragment.appendChild(child);
     }
+    console.log(fragment);
     return fragment;
   }
-  compile(el) {
-    // console.log(el);
-    // 得到子元素
-    var childNodes = el.childNodes;
-    var self = this;
 
-    var reg = /\{\{(.*)\}\}/;
+  compile(el) {
+    // 得到子元素
+    let childNodes = el.childNodes;
+    let self = this;
+
+    let reg = /\{\{(.*)\}\}/;
 
     childNodes.forEach((node) => {
-      var text = node.textContent;
-      text;
-      // console.log(node.nodeType);
-      // console.log(reg.test(text));
-      if (node.nodeType == 1) {
+      let text = node.textContent;
+      if (node.nodeType === 1) {
         self.compileElement(node);
-      } else if (node.nodeType == 3 && reg.test(text)) {
+      } else if (node.nodeType === 3 && reg.test(text)) {
+        // let text = node.textContent;
+        // console.log(text);
         let name = text.match(reg)[1];
         self.compileText(node, name);
       }
@@ -49,23 +49,20 @@ export default class Compile {
   }
 
   compileElement(node) {
-    // console.log(node);
-    // 这里的方便之处在于不是将HTML结构看做字符串，而是真正的属性列表
-    var nodeAttrs = node.attributes;
-    var self = this;
+    console.log(node);
+    // 这里是真正的属性列表
+    let nodeAttrs = node.attributes;
 
-    // 类数组对象变为数组
-    [].slice.call(nodeAttrs).forEach((attr) => {
-      // 这里就分析指令
-      var attrName = attr.name;
-      var value = attr.value;
-      // 指令都是v-开头的
-      var dir = attrName.substring(2);
+    Array.prototype.slice.call(nodeAttrs).forEach((attr) => {
+      // 分析指令
+      let attrName = attr.name;
+      let value = attr.value;
+      // 指令都是以v-开头的
+      let dir = attrName.substring(2);
 
       // 看看是不是指令
-      if (attrName.indexOf("v-") == 0) {
-        // v-开头的就是指令
-        if (dir == "model") {
+      if (attrName.indexOf("v-") === 0) {
+        if (dir === "model") {
           // console.log('发现了model指令', value);
           new Watcher(self.$vue, value, (value) => {
             node.value = value;
@@ -79,16 +76,15 @@ export default class Compile {
             self.setVueVal(self.$vue, value, newVal);
             v = newVal;
           });
-        } else if (dir == "if") {
-          // console.log('发现了if指令', value);
+        } else if (dir === "if") {
+          console.log("发现了if指令", value);
         }
       }
+      // console.log(dir)
     });
   }
-
+  
   compileText(node, name) {
-    // console.log('AA', name);
-    // console.log('BB', this.getVueVal(this.$vue, name));
     node.textContent = this.getVueVal(this.$vue, name);
     new Watcher(this.$vue, name, (value) => {
       node.textContent = value;
